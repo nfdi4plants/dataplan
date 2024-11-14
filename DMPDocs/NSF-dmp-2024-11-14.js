8bce811b5e9220013df0bc790874cb7b5695d574
Xiaoran Zhou <zxrzxr@gmail.com>
2024-11-14 18:50:35 +0100

    added NSF DMSP

--- DMPDocs/NSF-dmp-2024-11-14.js
+++ DMPDocs/NSF-dmp-2024-11-14.js
@@ -1,9 +1,9 @@
-BBSRC_dmp = {"BBSRC-dmp" : `<div id="BBSRC-dmp" class="">
+NSF_dmp = {"NSF-dmp" : `<div id="NSF-dmp" class="">
     <div class="content-page" id="text_start">
-        <span class="c2">Data management plan of $_PROJECTNAME for BBSRC</span>
+        <span class="c2">Data management and sharing plan of $_PROJECTNAME for U.S. National Science Foundation (NSF)</span>
         <ul class="c0 ">
 
-            <li class="c0 li-bullet-0"><span class="c0">Data Areas and Data Types <br> &ndash; $_PROJECTNAME will collect
+            <li class="c0 li-bullet-0"><span class="c0">Types of data, samples, physical collections, software, curriculum materials, and other materials. <br> &ndash; $_PROJECTNAME will collect
                     and/or generate the following types of raw data: #if$_GENETIC genetic data, #endif$_GENETIC  
                     #if$_GENOMIC genomic data, 
                     #if$_PANGENOMIC Pangenomic data, #endif$_PANGENOMIC 
@@ -33,7 +33,7 @@ BBSRC_dmp = {"BBSRC-dmp" : `<div id="BBSRC-dmp" class="">
             <span class="c0">We expect to generate raw data in the range of $_RAWDATA GB of data. The size of the
                 derived data will be about $_DERIVEDDATA GB.</span>
             <p class="c0"><span class="c0"></span></p>
-            <li class="c0 li-bullet-0"><span class="c0">Standards and Metadata <br> &ndash;&nbsp;                <span class="c1">
+            <li class="c0 li-bullet-0"><span class="c0">The standards to be used for data and metadata format and content<br> &ndash;&nbsp;                <span class="c1">
                 All datasets will be associated with unique identifiers and will be annotated with metadata. We will
                 use Investigation, Study, Assay (ISA) specification for metadata creation. $_PROJECTNAME will rely
                 on community standards plus additional recommendations applicable in the plant science, such as the
@@ -87,7 +87,37 @@ BBSRC_dmp = {"BBSRC-dmp" : `<div id="BBSRC-dmp" class="">
 
                 </span>
             </li>
-            <li class="c0 li-bullet-0"><span class="c0">Reuse of published data <br> &ndash; The project builds on
+            <li class="c0 li-bullet-0"><span class="c0">Policies for data access and sharing  <p class="c0">
+                        <span class="c1"> The data will not only initially benefit the
+                    $_PROJECTNAME partners, but will also be made available to selected stakeholders closely involved in
+                    the project, and then the scientific community working on $_STUDYOBJECT. $_DATAUTILITY In
+                    addition, the general public interested in $_STUDYOBJECT can also use the data after
+                    publication. The data will be disseminated according to $_PROJECTNAME&#39;s dissemination and
+                    communication plan, #if$_DATAPLANT which aligns with DataPLANT platform or other means #endif$_DATAPLANT.</span>
+                        <span class="c1">
+                            &ndash; Data will be made available via the $_PROJECTNAME platform or a public platform using a user-friendly frontend
+                            that allows data visualization. 
+
+                            #if$_DATAPLANT DataPLANT offers opensource data curation tools such as the <a target="_blank" href="https://nfdi4plants.github.io/nfdi4plants.knowledgebase/arcitect/">ARC management tool ARCitect </a>, command line tool <a target="_blank" href="https://nfdi4plants.github.io/nfdi4plants.knowledgebase/arc-commander/">ARCcommander </a>, <a target="_blank" href="https://github.com/nfdi4plants/nfdi4plants_ontology"> DataPLANT Biological Ontology (DPBO)</a>, <a target="_blank" href="https://nfdi4plants.github.io/nfdi4plants.knowledgebase/swate/">metadata annotation tool swate</a>, <a target="_blank" href="https://nfdi4plants.github.io/nfdi4plants.knowledgebase/resources/metadata-quiz/">the Metadata Quiz</a> and <a target="_blank" href="https://nfdi4plants.github.io/nfdi4plants.knowledgebase/resources/dataplan/">DataPLAN DMP generator</a>. #endif$_DATAPLANT
+                        </span>
+                    &ndash; #if$_EARLY Some raw data is made public as soon as it is collected and processed. #endif$_EARLY
+                    #if$_BEFOREPUBLICATION Relevant processed datasets are made public when the research findings
+                    are published. #endif$_BEFOREPUBLICATION #if$_ENDOFPROJECT At the end of the project, all data
+                    without embargo period will be published. #endif$_ENDOFPROJECT #if$_EMBARGO Data, which is
+                    subject to an embargo period, is not publicly accessible until the end of embargo
+                    period. #endif$_EMBARGO #if$_REQUEST Data is made available upon request, allowing controlled
+                    sharing while ensuring responsible use. #endif$_REQUEST #if$_IPISSUE IP issues will be checked
+                    before publication. #endif$_IPISSUE All consortium partners will be
+                    encouraged to make data available before publication, openly and/or under pre-publication
+                    agreements #if$_GENOMIC such as those started in Fort Lauderdale and set forth by the Toronto
+                    International Data
+                    Release Workshop #endif$_GENOMIC . This will be implemented as soon as IP-related checks are
+                    complete.
+                    </p></span></li>
+                    
+                    
+                    <li class="c0 li-bullet-0"><span class="c0">Policies and provisions for data reuse, redistribution and the production of derivatives <br> &ndash; Open public
+                data will be used whenever possible.  The project builds on
                     existing data sets and relies on them. #if$_RNASEQ For example, without a proper genomic
                     reference it is very difficult to analyze next-generation sequencing (NGS) data
                     sets. #endif$_RNASEQ It is also important to include existing data-sets on the expression and
@@ -97,26 +127,13 @@ BBSRC_dmp = {"BBSRC-dmp" : `<div id="BBSRC-dmp" class="">
                     EBI (European Bioinformatics Institute) and DDBJ (DNA Data Bank of Japan). Furthermore, prior
                     &#39;unstructured&#39; data in the form of publications and data contained therein will be used
                     for decision making.</span></li>
-        </ul>
-        <p class="c0 c0"><span class="c0"></span></p>
-        <ul class="c0 ">
-            <li class="c0 li-bullet-0"><span class="c0">Secondary Use <br> &ndash; The data will not only initially benefit the
-                    $_PROJECTNAME partners, but will also be made available to selected stakeholders closely involved in
-                    the project, and then the scientific community working on $_STUDYOBJECT. $_DATAUTILITY In
-                    addition, the general public interested in $_STUDYOBJECT can also use the data after
-                    publication. The data will be disseminated according to $_PROJECTNAME&#39;s dissemination and
-                    communication plan, #if$_DATAPLANT which aligns with DataPLANT platform or other means #endif$_DATAPLANT.</span></li>
-
-            <li class="c0 li-bullet-0"><span class="c0">Methods for Data Sharing  <p class="c0">
-                        <span class="c1">
-                            &ndash; Data will be made available via the $_PROJECTNAME platform or a public platform using a user-friendly frontend
-                            that allows data visualization. Besides this it will be ensured that data which can be
-                            stored in
-                            international discipline related repositories which use specialized technologies:
-                        
 
 
-                            <p class="c0">
+
+
+            <li class="c0 li-bullet-0"><span class="c0">Plans for archiving data, samples and other research products, and for preserving access to them <br>
+                    In addition to project related sharing platform, data will be stored in international discipline related repositories which use specialized technologies and preserve data for more than 10 years:
+                                 <p class="c0">
                                 <span class="c1 list-to-remove-comma"> #if$_GENETIC|$_GENOMIC|$_RNASEQ For genetic or genomic data: #if$_GENBANK NCBI-GenBank, #endif$_GENBANK #if$_ENA EBI-ENA, #endif$_ENA #endif$_GENETIC|$_GENOMIC|$_RNASEQ #if$_GENETIC #if$_SRA NCBI-SRA (Sequence Read Archive), #endif$_SRA #if$_ARRAYEXPRESS EBI-ArrayExpress, #endif$_ARRAYEXPRESS
                                     #if$_GEO NCBI-GEO (Gene Expression Omnibus), #endif$_GEO #endif$_GENETIC #if$_GENETIC|$_GENOMIC|$_RNASEQ . #endif$_GENETIC|$_GENOMIC|$_RNASEQ </span>
                             </p>
@@ -153,31 +170,7 @@ BBSRC_dmp = {"BBSRC-dmp" : `<div id="BBSRC-dmp" class="">
                             </p>
                             #if$_OTHEREP $_OTHEREP will also be used to store data and the data will be
                             processed there as well. #endif$_OTHEREP
-
-                            #if$_DATAPLANT DataPLANT offers opensource data curation tools such as the <a target="_blank" href="https://nfdi4plants.github.io/nfdi4plants.knowledgebase/arcitect/">ARC management tool ARCitect </a>, command line tool <a target="_blank" href="https://nfdi4plants.github.io/nfdi4plants.knowledgebase/arc-commander/">ARCcommander </a>, <a target="_blank" href="https://github.com/nfdi4plants/nfdi4plants_ontology"> DataPLANT Biological Ontology (DPBO)</a>, <a target="_blank" href="https://nfdi4plants.github.io/nfdi4plants.knowledgebase/swate/">metadata annotation tool swate</a>, <a target="_blank" href="https://nfdi4plants.github.io/nfdi4plants.knowledgebase/resources/metadata-quiz/">the Metadata Quiz</a> and <a target="_blank" href="https://nfdi4plants.github.io/nfdi4plants.knowledgebase/resources/dataplan/">DataPLAN DMP generator</a>. #endif$_DATAPLANT
-                        </span>
-                    </p></span></li> <li class="c0 li-bullet-0"><span class="c0">Proprietary Data <br> &ndash; Open public
-                data will be used whenever possible. </span></li>
-            <li class="c0 li-bullet-0"><span class="c0">Timeframes <br>
-                    &ndash; #if$_EARLY Some raw data is made public as soon as it is collected and processed. #endif$_EARLY
-                    #if$_BEFOREPUBLICATION Relevant processed datasets are made public when the research findings
-                    are published. #endif$_BEFOREPUBLICATION #if$_ENDOFPROJECT At the end of the project, all data
-                    without embargo period will be published. #endif$_ENDOFPROJECT #if$_EMBARGO Data, which is
-                    subject to an embargo period, is not publicly accessible until the end of embargo
-                    period. #endif$_EMBARGO #if$_REQUEST Data is made available upon request, allowing controlled
-                    sharing while ensuring responsible use. #endif$_REQUEST #if$_IPISSUE IP issues will be checked
-                    before publication. #endif$_IPISSUE All consortium partners will be
-                    encouraged to make data available before publication, openly and/or under pre-publication
-                    agreements #if$_GENOMIC such as those started in Fort Lauderdale and set forth by the Toronto
-                    International Data
-                    Release Workshop #endif$_GENOMIC . This will be implemented as soon as IP-related checks are
-                    complete.
                 </span></li>
-            <li class="c0 li-bullet-0"><span class="c0">Format of the Final Dataset <br> &ndash; Whenever possible, data
-                    will be stored in common and openly defined formats including all the necessary metadata. By
-                    default, no proprietary formats will be used. However, Microsoft Excel files (according to
-                    ISO/IEC 29500-1:2016) might be used as intermediates by the consortium#if$_DATAPLANT and by some
-                    Annotated Research Context (ARC) components#endif$_DATAPLANT. In addition, text files might be edited in text processor
-                    files, but will be shared as pdf.</span></li>
+            
     </div>
 </div>`}
